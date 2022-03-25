ae56edef807faa1850040b1565276f3b917c0446
MJebran <jebran.mustafa@gmail.com>
2022-03-24 21:01:54 -0600

    ApiHelper and api information is added  in the text file and Thunder

---
+++ backend/ApiHelper.cs
@@ -0,0 +1,21 @@
+using System.Net.Http.Headers;
+
+
+
+// This is to convet html to json
+
+namespace FlippingProperty;
+public static partial class ApiHelper
+{
+    public static HttpClient ApiClient { get; set; }
+
+    public static void startCline()
+    {
+        ApiClient = new HttpClient();
+
+        ApiClient.DefaultRequestHeaders.Accept.Clear();
+
+        ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+}
