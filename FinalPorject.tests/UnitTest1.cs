668c723398786bc863906e53fe9f041bd7169ed6
Mustafa <89949006+MJebran@users.noreply.github.com>
2022-04-20 18:04:37 -0600

    first  failing test

--- FinalPorject.tests/UnitTest1.cs
+++ FinalPorject.tests/UnitTest1.cs
@@ -7,11 +7,33 @@ namespace FinalProject.tests;
 
 public class Tests
 {
+    public HouseModel theHouse;
     [SetUp]
     public void Setup()
     {
         
     }
+    [Test]
+    public async void ApiDataSourceTest()
+    {
+        var ApiDataSourceVar = new ApiDataSource();
+        theHouse = await ApiDataSourceVar.GetPropertyData("135 W Center St", "Ephraim", "UT", "84627");
+        Assert.Pass();
+
+        // string Task<HouseModel> ApiDataSourcetest.GetPropertyData(string address, string city, string state, string zip_code);
+        // Assert.Equals(HouseProcessor.LoudHouse(address, city, state, zip_code));
+    }
+    // [Test]
+    // public void addressTest()
+    // {
+    //     Assert.AreEqual("135 W Center St", theHouse.data.address.formatted_street_address);
+    // }
+    // [Test]
+    // public void APICallTest()
+    // {
+    //     var HouseProcessor = new HouseModel();
+    //     FlippingProperty.
+    // }
 
     // [Test]
     // public void ApiHelperTest()
@@ -20,12 +42,12 @@ public class Tests
     //     HttpClient.StartClient();
     //     Assert.AreEqual(HttpClient.HttpClient, new MediaTypeWithQualityHeaderValue("application/json"));
     // }
-    [Test]
-    public void StaticFileDataSource()
-    {
-        var HouseModel GetPropertyData = new 
+    // [Test]
+    // public void StaticFileDataSource()
+    // {
+    //     var HouseModel GetPropertyData = new 
 
-    }
+    // }
 
     
 }
